6f4a4a411c4bf2dd50209957ce685b472f7755f5
Nadav Kavalerchik <nadavkav@gmail.com>
2014-07-28 00:03:37 +0300

    Hebrew translation (Still missing for M27)

---
+++ lang/he/oublog.php
@@ -0,0 +1,288 @@
+<?php
+
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Strings for component 'oublog', language 'he', branch 'MOODLE_26_STABLE'
+ *
+ * @package   oublog
+ * @copyright 1999 onwards Martin Dougiamas  {@link http://moodle.com}
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+$string['accessdenied'] = 'סליחה: אין גישת צפייה לעמוד זה.';
+$string['activeblogs'] = 'פעיל';
+$string['addcomment'] = 'שמירת התגובה';
+$string['addlink'] = 'הוספת קישור';
+$string['addpost'] = 'הוספת פרסום';
+$string['allowcomments'] = 'אפשרו תגובות';
+$string['allowcommentsmax'] = 'אפשרו תגובות';
+$string['atom'] = 'Atom';
+$string['atomfeed'] = 'הזנת Atom';
+$string['attachments'] = 'קבצים מצורפים';
+$string['bloginfo'] = 'מידע על יומן־רשת';
+$string['blogname'] = 'שם הבלוג (יומן־רשת)';
+$string['blogoptions'] = 'עדכון מאפייני יומן־הרשת';
+$string['blogsummary'] = 'הנחיות לתלמידים של יומן-הרשת';
+$string['comment'] = 'הוספת תגובה';
+$string['commentalert'] = 'דווח על תגובה';
+$string['commentonby'] = 'תגובות על <u>{$a->title}</u> על-ידי <u>{$a->author}</u>';
+$string['commentposts'] = 'הפרסומים שהגיבו להם הכי הרבה';
+$string['commentposts_info_alltime'] = 'הפרסומים עם מספר התגובות המירבי';
+$string['commentposts_info_thismonth'] = 'הפרסומים עם מספר התגובות המירבי בחודש האחרון';
+$string['commentposts_info_thisyear'] = 'הפרסומים עם מספר התגובות המירבי בשנה האחרונה';
+$string['comments'] = 'תגובות';
+$string['commentsby'] = 'תגובות של {$a}';
+$string['commentsfeed'] = 'רק תגובות';
+$string['comments_info_alltime'] = '{$a} הפרסומים עם מספר התגובות המירבי';
+$string['comments_info_thismonth'] = '{$a} הפרסומים עם מספר התגובות המירבי י בחודש האחרון';
+$string['comments_info_thisyear'] = '{$a} הפרסומים עם מספר התגובות המירבי בשנה האחרונה';
+$string['commentsnotallowed'] = 'איסור על קבלה/שליחת תגובות';
+$string['completioncomments'] = 'על המשתמש להגיב על פרסומים ביומן־רשת';
+$string['completioncommentsgroup'] = 'דרושות תגובות';
+$string['completioncommentsgroup_help'] = 'אם תאפשרו הגדרה זו, הבלוג יסומן שהושלם לסטודנט במידה והסטודנט ישאיר לפחות כמספר התגובות שנקוב.';
+$string['completionposts'] = 'על המשתמש לפרסם פרסומים ביומן־רשת';
+$string['completionpostsgroup'] = 'דרושים פרסומים';
+$string['completionpostsgroup_help'] = 'אם תאפשרו הגדרה זו, הבלוג יסומן שהושלם לסטודנט במידה והסטודנט יגיב לפחות כמספר התגובות שנקוב.';
+$string['computingguide'] = 'מדריך ליומני-רשת  OU';
+$string['computingguideurl'] = 'מדריך מחשוב URL';
+$string['computingguideurlexplained'] = 'הזינו את כתובת ה-URL עבור מדריך יומני-רשת  OU';
+$string['configmaxattachments'] = 'ברירת מחדל של המספר המירבי של קבצים מצורפים עבור כל פרסום בבלוג.';
+$string['configmaxbytes'] = 'הגודל המירבי של קובץ מצורף לבלוגים באתר כולו (מוגבל לקורס ולהגדרות מקומיות אחרות).';
+$string['confirmdeletecomment'] = 'האם אתם בטוחים שברצונכם למחוק תגובה זו?';
+$string['confirmdeletelink'] = 'האם אתם בטוחים שברצונכם למחוק קישור זה?';
+$string['confirmdeletepost'] = 'האם אתה בטוח כי ברצונך למחוק את הפרסום?';
+$string['couldnotaddcomment'] = 'לא היה ניתן להוסיף  תגובה';
+$string['couldnotaddlink'] = 'לא היה ניתן להוסיף קישור';
+$string['delete'] = 'מחיקה';
+$string['deletedby'] = 'נמחק ע"י {$a->fullname}, {$a->timedeleted}';
+$string['deleteglobalblog'] = 'אינכם רשאים למחוק את היומן־הרשת הכללי';
+$string['details'] = 'פרטים';
+$string['displayname_default'] = 'בלוג';
+$string['displayversion'] = 'גרסת יומן־רשת OUBlog מספר: <strong>{$a}</strong>';
+$string['edit'] = 'עריכה';
+$string['editlink'] = 'עריכת קישור';
+$string['editonsummary'] = 'עריכה  {$a-> תאריך עריכה }';
+$string['editpost'] = 'עדכון פרסום';
+$string['editsummary'] = 'נערך על-ידי {$a->editby}, {$a->editdate}';
+$string['error_alreadyapproved'] = 'התגובה כבר אושרה או נדחתה';
+$string['error_grouppubliccomments'] = 'אינכם רשאים להתיר הוספת תגובות של הציבור, בשעה שיומן-הרשת נמצא במצב של קבוצה';
+$string['error_noconfirm'] = 'הזינו את הכיתוב המודגש שלמעלה, באופן מדויק כפי שנמסר , לתוך תיבה זו.';
+$string['error_toomanycomments'] = 'פרסמתם מכתובת אינטרנט זו במשך השעה האחרונה, יותר מדי תגובות ביומן-הרשת . נא המתינו זמן מה, ולאחר מכן נסו שוב.';
+$string['error_unspecified'] = 'אין באפשרות המערכת להשלים את הבקשה, היות ואירעה שגיאה ({$})';
+$string['exportpostscomments'] = 'כל ההודעות והערותיהן הגלויות.';
+$string['exportuntitledpost'] = 'הודעה ללא כותרת';
+$string['externaldashboardadd'] = 'הוסיפו את היומן־רשת ללוח ההודעות האישי שלכם';
+$string['externaldashboardremove'] = 'הסירו את היומן־הרשת מלוח ההודעות האישי שלכם';
+$string['extranavolderposts'] = 'פרסומים ישנים:
+ {$a->מ-}- {$a->עד}';
+$string['extranavtag'] = 'סמנו: {$a}';
+$string['feedhelp'] = 'הזנות';
+$string['feeds'] = 'הזנות';
+$string['feedsnotenabled'] = 'הזנות לא אפשריות';
+$string['foruser'] = 'עבור {$a}';
+$string['globalblogmissing'] = 'יומן־הרשת הכללי חסר';
+$string['gradesupdated'] = 'הציונים עודכנו';
+$string['individualblogs'] = 'סוג בלוג (יומן־רשת)';
+$string['invalidblog'] = 'מספר הזיהוי של היומן־רשת אינו בתוקף';
+$string['invalidblogdetails'] = 'לא ניתן לאתר את פרטיי הפרסום  {$a}';
+$string['invalidcomment'] = 'מספר הזיהוי של התגובה אינו בתוקף';
+$string['invalidedit'] = 'מספר הזיהוי של העריכה אינו בתוקף';
+$string['invalidformat'] = 'סוג ההזנה חייב להיות atom או rss';
+$string['invalidlink'] = 'מספר הזיהוי של הקישור אינו בתוקף';
+$string['invalidpost'] = 'מספר הזיהוי של הפרסום אינו בתוקף';
+$string['invalidpostid'] = 'מספר הזיהוי של הפרסום אינו בתוקף';
+$string['invalidvisbilitylevel'] = 'מידת הזמינות {$a} אינה בתוקף';
+$string['invalidvisibility'] = 'מידת הזמינות אינה בתוקף';
+$string['lastcomment'] = '(אחרון על-ידי  {$a->fullname}, {$a->timeposted})';
+$string['links'] = 'קישורים נוספים';
+$string['logincomments'] = 'ממשתמשים רשומים';
+$string['maxattachments'] = 'מספר קבצים מצורפים מרבי';
+$string['maxattachmentsize'] = 'גודל קובץ מצורף מרבי';
+$string['maxvisibility'] = 'מידת הזמינות';
+$string['message'] = 'הודעה/פרסום';
+$string['moderated_addedcomment'] = 'הוספת תגובתכם התקבלה בהצלחה.
+ תגובה זו לא תופיע עד שתואשר על ידי מחבר הפרסום.';
+$string['moderated_approve'] = 'אשרו תגובה זו';
+$string['moderated_authorname'] = 'שמכם';
+$string['moderated_awaiting'] = 'תגובות הממתינות לאישור';
+$string['moderated_awaitingnote'] = 'תגובות אלו אינן גלויות למשתמשים האחרים, אלא אם כן אישרתם אותם כחברים. יש לזכור כי המערכת אינה יודעת את זהותם של המגיבים, ותגובות מהם עשויות לכלול קישורים אשר עלולים להזיק למחשב שלכם, במידה ותקישו עליהם. אם יש לכם ספק כלשהו לגבי תוכנם, אנא דחו התגובות. .';
+$string['moderated_confirm'] = 'אישור';
+$string['moderated_confirminfo'] = 'אנא הקישו בתיבה למטה
+<strong> <כן></strong>  , אם רצונכם להמשיך בפעילות.';
+$string['moderated_confirmvalue'] = 'כן';
+$string['moderated_emailsubject'] = 'תגובה לפרסום המחכה לאישור בבלוג: {$a->blog} ({$a->commenter})';
+$string['moderated_emailtext'] = 'זוהי הודעת דוא&quot;ל אוטומטית. אנא אל תענה.
+
+מישהו הוסיף תגובה לפוסט בבלוג שלך:
+ {$a->postlink}
+
+אתה צריך לאשר את התגובה לפני שהיא תופיע באופן ציבורי.
+
+המערכת אינה יודעת את זהותו של המגיב והערות עשויה לכלול קישורים אשר בעקבותיהם יכול להיווצר נזק למחשב שלך. אם אתה בספק, יש לדחות את התגובה. אם אתה מעוניין לאשר את התגובה, אתה למעשה לוקח אחריות על פרסומים בה.
+
+כדאי לוודא שהיא לא מכילה שום דבר שהוא בניגוד לכללים.
+
+-------------------------------------------------- ---------------------
+שם: {$a->commenter}
+ {$a->commenttitle} {$a->comment} *
+ {$a->approvetext}: {$a->approvelink} *
+ {$a->rejecttext}: {$a->rejectlink}
+
+אתה גם יכול להתעלם מהודעה זו. התגובה תידחה באופן אוטומטי לאחר 30 ימים. אם אתה מקבל יותר מדי הודעות דוא&quot;ל אלו, ייתכן שתרצו להגביל להעיר למשתמשים רשומים בלבד.
+
+* {$a->restrictposttext}: {$a->restrictpostlink} *  {$a->restrictblogtext}: {$a->restrictbloglink}';
+$string['moderated_info'] = 'היות ואינכם מחוברים,תגובתכם תופיע רק לאחר אישורה. במידה ויש לכם חשבון במערכת, אנא <a href=\'{$a}\'>
+ הרשמו לשם גישה מלאה
+ ליומן־רשת </a>.';
+$string['moderated_postername'] = 'שימוש בשם {$a}';
+$string['moderated_reject'] = 'דחו תגובה זו';
+$string['moderated_rejectedon'] = 'נדחה {$a}:';
+$string['moderated_restrictblog'] = 'הגבילו את מספר התגובות לכל הפרסומים שלכם ביומן־רשת זה.';
+$string['moderated_restrictblog_info'] = 'האם ברצונכם להגביל את התגובות על כל הפרסומים שלכם בבלוג ,כך שרק אנשים מחוברים למערכת, יוכלו להוסיף תגובות?';
+$string['moderated_restrictpage'] = 'הגבילו תגובות';
+$string['moderated_restrictpost'] = 'הגבילו תגובות בפרסום זה';
+$string['moderated_restrictpost_info'] = 'האם אתם מעוניינים להגביל את התגובות לפרסום זה,כך שרק אנשים מחוברים למערכת, יוכלו להוסיף תגובות?';
+$string['moderated_submitted'] = 'מחכה לאישור פרסום';
+$string['moderated_typicaltime'] = 'זמן הפעולה ערך בעבר, כ - {$a}.';
+$string['modulename'] = 'בלוג (יומן־רשת)';
+$string['modulename_help'] = 'רכיב הבלוג (יומן־רשת / OUBlog) מאפשר שימוש במספר סוגי בלוג שונים: יומניי־רשת (בלוגים) אישיים עבור כל משתמש, יומניי־רשת משותפים לכל התלמידים בקורס או לקבוצות משתמשים.';
+$string['modulenameplural'] = 'בלוגים (יומני־רשת OU Blog)';
+$string['mostcomments'] = 'התגובות המירביות';
+$string['mostposts'] = 'הפרסומים המירבים';
+$string['mustprovidepost'] = 'יש לספק מספר זיהוי לפרסום';
+$string['myparticipation'] = 'הפרסומים שלי';
+$string['ncomments'] = '{$a} תגובות';
+$string['newblogposts'] = 'פרסומים חדשים ביומן־רשת';
+$string['newcomment'] = 'תגובה חדשה';
+$string['newerposts'] = 'פרסומים חדשים יותר';
+$string['newpost'] = 'פרסום חדש {$a}';
+$string['no'] = 'לא';
+$string['noblogposts'] = 'לא קיימים פרסומים';
+$string['no_blogtogetheroringroups'] = 'בלוג משותף (לכל המשתמשים בקורס או לקבוצות משתמשים מוגדרות)';
+$string['nocomments'] = 'לא ניתן להוסיף תגובות';
+$string['noposts'] = 'אין פרסומים זמינים ב {$a}';
+$string['notaddpost'] = 'הוספת פרסום אינה אפשרית';
+$string['notaddpostnogroup'] = 'הוספת פרסום אינה אפשרית ללא קבוצה';
+$string['nousercomments'] = 'משתמש זה טרם הגיב לפרסומים בבלוג זה';
+$string['nouserposts'] = 'משתמש זה טרם פרסם תכנים בבלוג זה';
+$string['npending'] = '{$a} תגובות המחכות לאישור';
+$string['npendingafter'] = ', {$a} מחכים לאישור';
+$string['numbercomments'] = '{$a} תגובות';
+$string['numberposts'] = '{$a} פרסומים';
+$string['numberviews'] = '{$a} צפיות';
+$string['numposts'] = '{$a} פרסומים';
+$string['olderposts'] = 'פרסומים ישנים';
+$string['onecomment'] = '{$a} תגובה';
+$string['onepending'] = '{$a} תגובה מחכה לאישור';
+$string['onependingafter'] = ', {$a} מחכים לאישור';
+$string['onlyworkspersonal'] = 'עובד (מתאים) רק עם יומני-רשת אישיים';
+$string['oublog'] = 'יומן־רשת OUBlog';
+$string['oublog:addinstance'] = 'הוספת בלוג חדש';
+$string['oublogallpostslogin'] = 'הכרח התחברות בכל עמודי הפרסומים';
+$string['oublogallpostslogin_desc'] = 'אפשר הכרח התחברות לעמוד הפרסומים של אתר הבלוג האישי.  כאשר מאופשר, רק משתמשים רשומים ומחוברים למערכת יוכלו לצפות את הקישור לעמוד זה.';
+$string['oublog:audit'] = 'תצוגת פרסומים שנמחקו וגרסאות ישנות של פרסומים.';
+$string['oublog:comment'] = 'הגיבו על פרסום';
+$string['oublog:contributepersonal'] = 'פרסמו והגיבו ביומני-רשת אישיים';
+$string['oublog:exportownpost'] = 'ייצוא פרסום שלכם';
+$string['oublog:exportpost'] = 'ייצוא פרסום';
+$string['oublog:exportposts'] = 'יצוא פרסומים';
+$string['oublog:grade'] = 'מתן ציון על פעילות המשתמשים בבלוג';
+$string['oublogintro'] = 'הנחייה לתלמידים';
+$string['oublog_managealerts'] = 'ניהול התראות של פרסומים או תגובות שדווחו';
+$string['oublog:managecomments'] = 'ניהול תגובות';
+$string['oublog:managelinks'] = 'ניהול קישורים';
+$string['oublog:manageposts'] = 'ניהול פרסומים';
+$string['oublog:post'] = 'צרו פרסום חדש';
+$string['oublog:view'] = 'תצוגת פרסומים';
+$string['oublog:viewindividual'] = 'תצוגת יומני־רשת אישיים';
+$string['oublog:viewparticipation'] = 'צפיה בפעילות המשתמשים בבלוג';
+$string['oublog:viewpersonal'] = 'תצוגת פרסומים ביומני-רשת אישיים';
+$string['oublog:viewprivate'] = 'צפיה בפרסומים פרטיים בבלוגים אישיים';
+$string['overviewnumentrylog'] = 'כניסות מאז תחילת הפעילות ביומן־רשת';
+$string['overviewnumentrylog1'] = 'כניסה מאז תחילת הפעילות ביומן־רשת';
+$string['overviewnumentryvw'] = 'כניסות מאז הצפייה האחרונה שלכם ביומן־רשת';
+$string['overviewnumentryvw1'] = 'כניסה מאז הצפייה האחרונה שלכם ביומן־רשת';
+$string['participation'] = 'השתתפות';
+$string['participationbyuser'] = 'דוח:השתתפות לפי משתמש';
+$string['permalink'] = 'קישור קבוע';
+$string['personalblognotsetup'] = 'יומני-רשת אישיים, עדיין לא הוגדרו';
+$string['pluginadministration'] = 'ניהול בלוג (יומן־רשת)';
+$string['pluginname'] = 'בלוג (יומן־רשת OUBlog)';
+$string['postalert'] = 'דיווח פרסום';
+$string['postauthor'] = 'מחבר הפרסום';
+$string['postdate'] = 'תאריך פרסום';
+$string['postedby'] = 'ע"י {$a}';
+$string['postedbymoderated'] = 'על ידי {$a->commenter} (מאושר על ידי {$a->approver}, {$a->approvedate})';
+$string['postedbymoderatedaudit'] = 'על ידי {$a->commenter} [{$a->ip}] (מאושר על ידי {$a->approver}, {$a->approvedate})';
+$string['posts'] = 'פרסומים';
+$string['postsby'] = 'פרסומים על-ידי {$a}';
+$string['posts_info_alltime'] = '{$a} עם הכי הרבה פרסומים';
+$string['posts_info_thismonth'] = '{$a} עם הכי הרבה פרסומים בחודש האחרון';
+$string['posts_info_thisyear'] = '{$a} עם הכי הרבה פרסומים בשנה האחרונה';
+$string['posttime'] = 'זמן פרסום';
+$string['posttitle'] = 'כותרת הפרסום';
+$string['publiccomments'] = 'מכולם (גם אם לא נרשמו)';
+$string['publiccomments_info'] = 'כאשר מישהו שאינו מחובר ,מוסיף תגובה ביומן־רשת, תקבלו הודעה בדוא"ל ותוכלו להחליט אם לאשר או לדחות את אותה תגובה.אפשרות בחירה זו נחוצה למניעת דואר זבל.';
+$string['re'] = 'תגובה: {$a}';
+$string['removeblogs'] = 'הסר את כל הפרסומים בבלוג';
+$string['reportingemail'] = 'דיווח כתובות דוא"ל';
+$string['rss'] = 'RSS';
+$string['rssfeed'] = 'הזנת RSS';
+$string['savegrades'] = 'שמירת ציונים';
+$string['searchblogs'] = 'חיפוש';
+$string['searchthisblog'] = 'חפש את זה: {$a}';
+$string['separateindividual'] = 'אישי (אינו מאפשר צפייה)';
+$string['separateindividualblogs'] = 'בלוגים (יומני־רשת) אישיים פרטיים';
+$string['siteentries'] = 'תצוגת כניסות לאתר';
+$string['summary'] = 'הנחיות לתלמידים';
+$string['tags'] = 'תוויות';
+$string['tagsfield'] = 'תוויות (מופרדות על-ידי תו "פסיק")';
+$string['tags_help'] = 'ישנם תגיות או תוויות שתעזורנה לך למצוא ולקטלג פרסומים';
+$string['tagupdatefailed'] = 'נכשלה פעולת עדכון התוויות';
+$string['timefilter_alltime'] = 'כל הזמן';
+$string['timefilter_close'] = 'הסתר את האפשרויות';
+$string['timefilter_label'] = 'תקופת זמן';
+$string['timefilter_open'] = 'הצגת האפשרויות';
+$string['timefilter_submit'] = 'עדכון';
+$string['timefilter_thismonth'] = 'בחודש האחרון';
+$string['timefilter_thisyear'] = 'בשנה האחרונה';
+$string['title'] = 'כותרת';
+$string['untitledcomment'] = 'תגובה ללא שם';
+$string['untitledpost'] = 'פרסום ללא שם';
+$string['url'] = 'כתובת אינטרנט מלאה';
+$string['usergrade'] = 'ציוניי משתמש';
+$string['userparticipation'] = 'פעילות משתמש';
+$string['viewallusers'] = 'תצוגת כל המשתמשים';
+$string['viewallusersingroup'] = 'תצוגת כל המשתמשים בקבוצה';
+$string['viewblogdetails'] = 'תצוגת מאפייני היומן־רשת';
+$string['viewblogposts'] = 'חזרה ליומן־רשת';
+$string['viewedit'] = 'תצוגת עריכה';
+$string['views'] = 'סך-כל צפיות ב{$a} הזה:';
+$string['visibility'] = 'מי רשאי לצפות בזה?';
+$string['visibleblogusers'] = 'אפשרות צפייה רק לחברים ביומן־רשת זה';
+$string['visiblecourseusers'] = 'אפשרות צפייה למשתתפיי קורס זה';
+$string['visibleindividual'] = 'אישי (כולל אפשרות צפייה)';
+$string['visibleindividualblogs'] = 'בלוגים (יומני־רשת) אישיים, הניתנים לצפייה על ידי שאר המשתמשים בקורס.';
+$string['visibleloggedinusers'] = 'אפשרות צפייה לכל אלו המחוברים למערכת';
+$string['visiblepublic'] = 'אפשרות צפייה ציבורית (גם למי שאינו מחובר למערכת)';
+$string['visibleyou'] = 'אפשרות צפייה רק למחבר יומן־הרשת (פרטי)';
+$string['visits'] = 'הצפיות המירביות';
+$string['visits_info_active'] = '{$a} אקטיבי (מכיל פרסום בחודש האחרון) עם המספר הגדול ביותר של צפיות (מבקרים).';
+$string['visits_info_alltime'] = '{$a} עם המספר הגדול ביותר של צפיות (מבקרים).';
+$string['yes'] = 'כן';
